4d3be8b413f0a50bc48117c9f0b75b7c99077da6
Nimrod Lilik <liliknimrod@gmail.com>
2023-07-26 17:47:39 +0200

    Changes

--- src/main/java/net/turbsi/mccourse/MCCourseMod.java
+++ src/main/java/net/turbsi/mccourse/MCCourseMod.java
@@ -2,6 +2,8 @@ package net.turbsi.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.turbsi.mccourse.block.ModBlocks;
+import net.turbsi.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/turbsi/mccourse/item/ModItems.java
@@ -0,0 +1,37 @@
+package net.turbsi.mccourse.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.turbsi.mccourse.MCCourseMod;
+import net.turbsi.mccourse.block.ModBlocks;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby",
+            new Item(new FabricItemSettings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    private static void itemGroupIngredients(FabricItemGroupEntries entries){
+        entries.add(RUBY);
+        entries.add(RAW_RUBY);
+
+        entries.add(ModBlocks.RUBY_BLOCK);
+    }
+
+    public static void registerModItems(){
+        MCCourseMod.LOGGER.info("Registering Mod Items for " + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::itemGroupIngredients);
+    }
+}
